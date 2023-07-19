3575fc241b2239046de62bdbadbd52fe458cfc83
azwanazlan <azwanazlan2809@gmail.com>
2023-07-19 10:18:00 +0800

    add

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.get("/" , (req, res) => {
 testDatabaseConnection()
   .then(() => {
     // Start the server after successful database connection
-    const port = 3000; // Choose a suitable port number
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
