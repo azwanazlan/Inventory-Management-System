e051f7106ad714377e6b55800fd16ab6a83c419f
azwanazlan <azwanazlan2809@gmail.com>
2023-07-22 14:59:59 +0800

    aa

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -5,25 +5,34 @@ const itemController = require('../Controller/ItemController');
 const employeeController = require('../Controller/EmployeeController');
 const agentController = require('../Controller/AgentController');
 
-//GET ALL ITEMS
+router.get('/', (req,res) => {
+    const welcomeMessage = 'Welcome';
+    const environmentVariables = process.env;
+
+    res.json({
+        message: welcomeMessage,
+        environmentVariables: environmentVariables
+    });
+});
+//ITEMS
 router.get('/items', itemController.getAllItems);
-
 router.get('/items/:id', itemController.getItembyId);
-
 router.post('/items', itemController.createAnItem);
-
 router.patch('/items/:id', itemController.updateAnItem);
-
 router.delete('/items/:id', itemController.deleteItem);
 
-router.get('/employee', employeeController.getAllItems);
-
+//EMPLOYEE
+router.get('/employee', employeeController.getAllEmployee);
 router.get('/employee/:id', employeeController.getEmployeeById);
-
 router.post('/employee', employeeController.createAnEmployee);
-
 router.patch('/employee/:id', employeeController.updateAnEmployee);
-
 router.delete('/employee/:id', employeeController.deleteAnEmployee);
+
+//AGENT
+router.get('/agent', agentController.getAllAgents);
+router.get('/agent/:id', agentController.getAgentbyId);
+router.post('/agent', agentController.createAnAgent);
+router.patch('/agent/:id', agentController.updateAnAgent);
+router.delete('/agent/:id', agentController.deleteAgent);
 
 module.exports = router;
--- seed.js
+++ seed.js
@@ -1,65 +1,74 @@
-Object.keys(require.cache).forEach((key) => {
-  delete require.cache[key];
-});
-
-
 const { sequelize } = require('./Services/db');
 const Item = require('./Models/Item');
 const Employee = require('./Models/Employee');
 const Agent = require('./Models/Agent');
+const { faker } = require('@faker-js/faker');
+const readline = require('readline');
 
 // Replace 'your_database_name', 'your_username', 'your_password', and 'your_host' with your actual database credentials
 sequelize.sync()
   .then(async () => {
     console.log('Database connection has been established successfully.');
-    // Generate sample data and add it to the 'Product' table
-    await Item.bulkCreate([
-      {
-        name: 'Product 1',
-        price: 10.99,
-        quantity: 50,
-        description: 'Sample product 1',
-        category: 'Category 1',
-      },
-      {
-        name: 'Product 2',
-        price: 19.99,
-        quantity: 30,
-        description: 'Sample product 2',
-        category: 'Category 2',
-      },
-    ]);
-   // Generate sample data and add it to the 'Employee' table
-    await Employee.bulkCreate([
-      {
-        firstName: 'Michael',
-        lastName: 'Johnson',
-      },
-      {
-        firstName: 'Emily',
-        lastName: 'Brown',
-      },
-    ]);
-   // Generate sample data and add it to the 'Agent' table
-    await Agent.bulkCreate([
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        // Add other properties as needed
-      },
-      {
-        firstName: 'Jane',
-        lastName: 'Smith',
-        // Add other properties as needed
-      },
-      // Add more sample data as needed
-    ]);
-  })
-  .then(() => {
-    console.log('Sample data has been added to the table.');
-    process.exit(0); // Exit the script after adding data
+    const r1 = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    r1.question('Enter how much row of data you want to add into DB: ', (count) => {
+      const rowCount = parseInt(count, 10);
+      console.log(`${rowCount} row(s) of data will be generated...`);
+      r1.close();
+      generateSampleDataIntoDB(rowCount)
+        .then(() => {
+          console.log('Sample data has been added to the table.');
+          process.exit(0); // Exit the script after adding data
+        })
+        .catch((err) => {
+          console.error('Error seeding the database:', err);
+          process.exit(1); // Exit with an error code in case of an error
+        });
+    });
   })
   .catch((err) => {
-    console.error('Error seeding the database:', err);
-    process.exit(1); // Exit with an error code in case of an error
+    console.error('Error connecting to the database:', err);
+    process.exit(1); // Exit with an error code in case of a database connection error
   });
+
+
+const generateSampleDataIntoDB = async (count) => {
+  try {
+    for (let i = 0; i < count; i++) {
+      // Generate sample data and add it to the 'Item' table
+      await Item.create({
+        name: faker.lorem.word(),
+        price: faker.finance.amount({
+          min: 10,
+          max: 1000
+        }),
+        quantity: faker.number.bigInt({
+          min: 1,
+          max: 1000
+        }),
+        description: faker.lorem.words(4),
+        category: faker.lorem.word(),
+      });
+
+      // Generate sample data and add it to the 'Employee' table
+      await Employee.create({
+        firstName: faker.lorem.word(),
+        lastName: faker.lorem.word(),
+      });
+
+      // Generate sample data and add it to the 'Agent' table
+      await Agent.create({
+        firstName: faker.lorem.word(),
+        lastName: faker.lorem.word(),
+      });
+    }
+  } catch (error) {
+    console.error('Error generating and adding sample data:', error);
+  }
+};
+  
+
+  
